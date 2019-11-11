1b646589b07068801649225a33599a864b13fa27
Alexandra <alexandraga.ag@gmail.com>
2019-11-11 21:47:52 +0200

    add customer loyality points

---
+++ Controller/Customer/Index.php
@@ -0,0 +1,8 @@
+<?php
+namespace PHP10E2\LoyaltyPoints\Controller\Customer;
+class Index extends \Magento\Framework\App\Action\Action {
+    public function execute() {
+        $this->_view->loadLayout();
+        $this->_view->renderLayout();
+    }
+}

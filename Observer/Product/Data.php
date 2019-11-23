d925a499f39d377c42d61235ab31c45838da872f
mihail.bakhmut <mihail.bahmut@gmail.com>
2019-11-23 15:15:39 +0200

    observer get referral link from cookie

--- Observer/Product/Data.php
+++ Observer/Product/Data.php
@@ -4,18 +4,44 @@ namespace PHP10E2\LoyaltyPoints\Observer\Product;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use PHP10E2\LoyaltyPoints\Block\Info;
+use PHP10E2\LoyaltyPoints\Controller\Referral\Get;
 
 class Data implements ObserverInterface
 {
+    /**
+     * @var Get
+     */
+    public $get;
 
-     /**
+    /**
+     * @var Info
+     */
+    public $blockInfo;
+
+    /**
+     * Data constructor.
+     * @param Get $get
+     * @param Info $blockInfo
+     */
+    public function __construct(Get $get, Info $blockInfo)
+    {
+        $this->get = $get;
+        $this->blockInfo = $blockInfo;
+    }
+
+    /**
      * @param Observer $observer
      */
     public function execute(Observer $observer)
     {
         $order = $observer->getEvent()->getOrder();
         $orderId = $order->getId();
-        print_r("Catch event successfully !");
+        $encodedId = $this->get->getFromCookie();
+        $decodedId= $this->blockInfo->decryptData($encodedId, 'key');
+
+        print_r("Catch event successfully : " . $decodedId);
+
         //exit;
     }
 }
